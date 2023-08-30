0c126189d14907e2e3e6ff85819c3d35482a3841
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2023-08-30 10:45:08 +0530

    hardhat gas repoter added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("./tasks/block-numbers");
+require("hardhat-gas-reporter");
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "http://0.0.0.0:6737";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "apiKey";
+const COINMARKET_API_KEY = process.env.COINMARKET_API_KEY || "keyApi";
 
-/** @type import('hardhat/config').HardhatUserConfig */
+/*
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -25,5 +29,13 @@ module.exports = {
   solidity: "0.8.19",
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gasReport.txt",
+    noColors: true,
+    currency: "INR",
+    coinmarketcap: COINMARKET_API_KEY,
+    token: "MATIC",
   },
 };
